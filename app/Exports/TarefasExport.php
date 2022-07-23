913d7b5e02aac618142aa8429e71b0c804727772
Leonardo Lopes <leonardolivelopes2@gmail.com>
2022-07-22 21:29:42 -0300

    data formated

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class TarefasExport implements FromCollection, WithHeadings
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -20,5 +21,18 @@ class TarefasExport implements FromCollection, WithHeadings
     public function headings(): array
     {
         return ['ID', 'USER ID', 'TAREFA', 'DATA LIMITE CONCLUSÃO', 'DATA CRIAÇÃO', 'DATA ATUALIÇÃO'];
+    }
+
+    // manipualando dados
+    public function map($tarefa): array
+    {
+        return [
+            $tarefa->id,
+            '#' . $tarefa->user_id . ' ' . $tarefa->user->name,
+            $tarefa->tarefa,
+            $tarefa->data_limite_formatada,
+            $tarefa->created_at->format('d/m/Y H:i:s'),
+            $tarefa->updated_at->format('d/m/Y H:i:s')
+        ];
     }
 }
